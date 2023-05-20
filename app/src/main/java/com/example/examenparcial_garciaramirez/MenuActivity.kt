2ad2da30b2c9d5f1cd54b31603816003800470d8
laboratorioesan <M1guel21gr>
2023-05-20 10:00:02 -0500

    Pregunta 1

--- app/src/main/java/com/example/examenparcial_garciaramirez/MenuActivity.kt
+++ app/src/main/java/com/example/examenparcial_garciaramirez/MenuActivity.kt
@@ -1,11 +1,25 @@
 package com.example.examenparcial_garciaramirez
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val btnIMC: Button = findViewById(R.id.btnIMC)
+        val btnSZ: Button = findViewById(R.id.btnSZ)
+        val btnGen: Button = findViewById(R.id.btnGen)
+
+        val btnRegresar: Button = findViewById(R.id.btnRegresar)
+
+        btnRegresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
